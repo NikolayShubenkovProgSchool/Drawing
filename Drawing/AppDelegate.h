e0b32d68d83851c0b2afb4f14edbbc7ed4f87e16
Nikolay Shubenkov <n.shubenkov@gmail.com>
2014-09-12 18:27:39 +0400

    Initial Commit

---
+++ Drawing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drawing
+//
+//  Created by n.shubenkov on 12/09/14.
+//  Copyright (c) 2014 n.shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Drawing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Drawing
+//
+//  Created by n.shubenkov on 12/09/14.
+//  Copyright (c) 2014 n.shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
